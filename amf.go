1238236f9d325cca3f460e302e81618ce0c1631d
pwbh <127856937+pwbh@users.noreply.github.com>
2023-05-12 10:54:16 +0300

    Continue working on AMF decoding started getting values, understood the structure of AMF0

--- amf.go
+++ amf.go
@@ -1,21 +1,23 @@
 package rtimpus
 
+import "fmt"
+
 type AMF0 byte
 
 const (
-	Number       = AMF0(0x00)
-	Boolean      = AMF0(0x01)
-	String       = AMF0(0x02)
-	Object       = AMF0(0x03)
-	Null         = AMF0(0x05)
-	ECMAArray    = AMF0(0x08)
-	ObjectEnd    = AMF0(0x09)
-	StrictArray  = AMF0(0x0a)
-	Date         = AMF0(0x0b)
-	LongString   = AMF0(0x0c)
-	XMLDocument  = AMF0(0x0f)
-	TypedObject  = AMF0(0x10)
-	SwitchToAMF3 = AMF0(0x11)
+	AMF0Number       = AMF0(0x00)
+	AMF0Boolean      = AMF0(0x01)
+	AMF0String       = AMF0(0x02)
+	AMF0Object       = AMF0(0x03)
+	AMF0Null         = AMF0(0x05)
+	AMF0ECMAArray    = AMF0(0x08)
+	AMF0ObjectEnd    = AMF0(0x09)
+	AMF0StrictArray  = AMF0(0x0a)
+	AMF0Date         = AMF0(0x0b)
+	AMF0LongString   = AMF0(0x0c)
+	AMF0XMLDocument  = AMF0(0x0f)
+	AMF0TypedObject  = AMF0(0x10)
+	AMF0SwitchToAMF3 = AMF0(0x11)
 )
 
 type Value struct {
@@ -26,22 +28,85 @@ type Value struct {
 type AMF0Result struct {
 	Command       string
 	TransactionID uint32
-	Payload       map[string]Value
+	Payload       []Value
 }
 
 func UnmarshalAMF0(message []byte) *AMF0Result {
+	result := new(AMF0Result)
+
+	phase := 0
 	start := 0
 	end := 0
 
-	for end < len(message) {
-		if isAmfType(message[end]) {
-			end += 1
-		} else {
+	for len(message) > end {
+		offset, ok := getFoundOffset(message, end)
+
+		if ok && end > start {
+			if phase == 0 {
+				fmt.Println(message[start:end])
+			} else if phase == 1 {
+				fmt.Println(message[start:end])
+			} else {
+				fmt.Println(string(message[start:end]))
+			}
+
+			start = end
+			phase++
 
 		}
+
+		if offset > 0 {
+			start += offset
+			end += offset + 1
+		} else {
+			end++
+		}
 	}
+
+	return result
+}
+
+func getFoundOffset(message []byte, currentIndex int) (int, bool) {
+	a := isAmfType(message[currentIndex])
+
+	if !a {
+		return 0, false
+	}
+
+	b := false
+
+	if len(message) > currentIndex+1 {
+		b = message[currentIndex+1] == byte(AMF0Number)
+	}
+
+	if a && b {
+		return 2, true
+	}
+
+	return 1, true
 }
 
 func isAmfType(b byte) bool {
-	return (b > 0x00 && b <= 0x11) && b != 0x04 && b != 0x0d && b != 0x0e
+	amf0Types :=
+		[]byte{0x00,
+			0x01,
+			0x02,
+			0x03,
+			0x05,
+			0x08,
+			0x09,
+			0x0a,
+			0x0b,
+			0x0c,
+			0x0f,
+			0x10,
+			0x11}
+
+	for i := 0; i < len(amf0Types)-1; i++ {
+		if amf0Types[i] == b {
+			return true
+		}
+	}
+
+	return false
 }
