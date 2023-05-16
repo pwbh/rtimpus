5cd8ec58afd1fcf319eb88e70de8a20e11a0e2c5
pwbh <127856937+pwbh@users.noreply.github.com>
2023-05-16 15:15:18 +0300

    Tweak encoder and add small test

--- amf/encoder.go
+++ amf/encoder.go
@@ -16,21 +16,31 @@ func NewAMF0Encoder(writer io.Writer) *AMF0Encoder {
 }
 
 func (e *AMF0Encoder) encodeNumber(num float64) error {
-	data := make([]byte, 1, 9)
-	data[0] = AMF0NumberMarker
-	binary.BigEndian.PutUint64(data[1:], math.Float64bits(num))
-	_, err := e.writer.Write(data)
+	buf := make([]byte, 9)
+	buf[0] = AMF0NumberMarker
+	binary.BigEndian.PutUint64(buf[1:], math.Float64bits(num))
+	_, err := e.writer.Write(buf)
 	return err
 }
 
 func (e *AMF0Encoder) encodeString(str string) error {
 	length := len(str)
-	data := make([]byte, 3, length+3)
-	data[0] = AMF0StringMarker
-	binary.BigEndian.PutUint16(data[:1], uint16(length))
-	data = append(data, str...)
-	_, err := e.writer.Write(data)
+	buf := make([]byte, 3, length+3)
+	buf[0] = AMF0StringMarker
+	binary.BigEndian.PutUint16(buf[1:], uint16(length))
+	buf = append(buf, str...)
+	_, err := e.writer.Write(buf)
 	return err
+}
+
+func (e *AMF0Encoder) encodeArray(arr []uint32) error {
+	length := len(arr) * 4
+	buf := make([]byte, length+1)
+	buf[0] = AMF0EcmaArrayMarker
+	for i, e := range arr {
+		binary.BigEndian.AppendUint32(buf[i*4+1:i*4+5], e)
+	}
+	return nil
 }
 
 func (e *AMF0Encoder) encodeBool(v bool) error {
@@ -38,8 +48,8 @@ func (e *AMF0Encoder) encodeBool(v bool) error {
 	if v {
 		value = 1
 	}
-	data := []byte{AMF0BooleanMarker, value}
-	_, err := e.writer.Write(data)
+	buf := []byte{AMF0BooleanMarker, value}
+	_, err := e.writer.Write(buf)
 	return err
 }
 
@@ -53,6 +63,10 @@ func (e *AMF0Encoder) encodeObject(obj Object) error {
 			return err
 		}
 		switch v := v.(type) {
+		case bool:
+			if err := e.encodeBool(v); err != nil {
+				return err
+			}
 		case Object:
 			if err := e.encodeObject(v); err != nil {
 				return err
@@ -65,8 +79,10 @@ func (e *AMF0Encoder) encodeObject(obj Object) error {
 			if err := e.encodeNumber(v); err != nil {
 				return err
 			}
-		case []uint32:
-			// need to encode arrays once encodeArray is ready
+		case []uint32: // Probably will not work correctly yet
+			if err := e.encodeArray(v); err != nil {
+				return err
+			}
 		default:
 			return errors.New("type is not recognized")
 		}
@@ -76,5 +92,41 @@ func (e *AMF0Encoder) encodeObject(obj Object) error {
 		return err
 	}
 
+	return nil
+}
+
+func (e *AMF0Encoder) Encode(value interface{}) error {
+	switch v := value.(type) {
+	case bool:
+		if err := e.encodeBool(v); err != nil {
+			return err
+		}
+	case Object:
+		if err := e.encodeObject(v); err != nil {
+			return err
+		}
+	case string:
+		if err := e.encodeString(v); err != nil {
+			return err
+		}
+	case int:
+		if err := e.encodeNumber(float64(v)); err != nil {
+			return err
+		}
+	case float32:
+		if err := e.encodeNumber(float64(v)); err != nil {
+			return err
+		}
+	case float64:
+		if err := e.encodeNumber(v); err != nil {
+			return err
+		}
+	case []uint32: // Probably will not work correctly yet
+		if err := e.encodeArray(v); err != nil {
+			return err
+		}
+	default:
+		return errors.New("type is not recognized")
+	}
 	return nil
 }
---
+++ amf_test.go
@@ -0,0 +1,30 @@
+package rtimpus
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+
+	"github.com/pwbh/rtimpus/amf"
+)
+
+func TestAMFEncodesMessage(t *testing.T) {
+	buf := bytes.NewBuffer([]byte{})
+	amf0Encoder := amf.NewAMF0Encoder(buf)
+	if err := amf0Encoder.Encode("_result"); err != nil {
+		t.Fatalf(`amf0Encoder.Encode("_result") =  %v`, err)
+	}
+	if err := amf0Encoder.Encode("Hello World!"); err != nil {
+		t.Fatalf(`amf0Encoder.Encode("Hello World!") =  %v`, err)
+	}
+	if err := amf0Encoder.Encode("This is another test"); err != nil {
+		t.Fatalf(`amf0Encoder.Encode("This is another test") =  %v`, err)
+	}
+	if err := amf0Encoder.Encode(10_252); err != nil {
+		t.Fatalf(`amf0Encoder.Encode("This is another test") =  %v`, err)
+	}
+	if err := amf0Encoder.Encode(50); err != nil {
+		t.Fatalf(`amf0Encoder.Encode("This is another test") =  %v`, err)
+	}
+	fmt.Println(buf.Bytes())
+}
