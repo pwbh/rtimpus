6aeed14810f8566a4a8d3a23b4a6f36707ee1440
pwbh <127856937+pwbh@users.noreply.github.com>
2023-05-18 19:42:15 +0300

    Simple test all fields passing

--- amf/decoder_test.go
+++ amf/decoder_test.go
@@ -6,14 +6,14 @@ import (
 )
 
 const (
-	FIRST_FIELD  = "_result"
-	SECOND_FIELD = "Hello World!"
-	THIRD_FIELD  = "This is another test"
-	FOURTH_FIELD = 10_252
-	FIFTH_FIELD  = 50
+	SIMPLE_MESSAGE_FIRST_FIELD  = "_result"
+	SIMPLE_MESSAGE_SECOND_FIELD = "Hello World!"
+	SIMPLE_MESSAGE_THIRD_FIELD  = "This is another test"
+	SIMPLE_MESSAGE_FOURTH_FIELD = float64(10_252)
+	SIMPLE_MESSAGE_FIFTH_FIELD  = float64(50)
 )
 
-func encodedMessage() ([]byte, error) {
+func amf0SimpleMessage() ([]byte, error) {
 	buf := bytes.NewBuffer([]byte{})
 	amf0Encoder := NewAMF0Encoder(buf)
 	if err := amf0Encoder.Encode("_result"); err != nil {
@@ -35,20 +35,45 @@ func encodedMessage() ([]byte, error) {
 }
 
 func TestAMF0DecoderDecodesSimpleMessage(t *testing.T) {
-	message, err := encodedMessage()
+	message, err := amf0SimpleMessage()
 	if err != nil {
 		t.Fatalf("coudln't encode a AMF0 message for decoder tests")
 	}
 	buf := bytes.NewBuffer(message)
 	decoder := NewAMF0Decoder(buf)
-
 	firstField, err := decoder.Decode()
-
 	if err != nil {
 		t.Fatalf("err while decoding: %v", err)
 	}
-
-	if firstField.(string) != FIRST_FIELD {
-		t.Fatalf("Decoded firstField != FIRST_FIELd")
+	if firstField.(string) != SIMPLE_MESSAGE_FIRST_FIELD {
+		t.Fatalf("Decoded firstField != SIMPLE_MESSAGE_FIRST_FIELD")
+	}
+	secondField, err := decoder.Decode()
+	if err != nil {
+		t.Fatalf("err while decoding: %v", err)
+	}
+	if secondField.(string) != SIMPLE_MESSAGE_SECOND_FIELD {
+		t.Fatalf("Decoded secondField != SIMPLE_MESSAGE_SECOND_FIELD")
+	}
+	thirdField, err := decoder.Decode()
+	if err != nil {
+		t.Fatalf("err while decoding: %v", err)
+	}
+	if thirdField.(string) != SIMPLE_MESSAGE_THIRD_FIELD {
+		t.Fatalf("Decoded thirdField != SIMPLE_MESSAGE_THIRD_FIELD")
+	}
+	fourthField, err := decoder.Decode()
+	if err != nil {
+		t.Fatalf("err while decoding: %v", err)
+	}
+	if fourthField.(float64) != SIMPLE_MESSAGE_FOURTH_FIELD {
+		t.Fatalf("Decoded fourthField != SIMPLE_MESSAGE_FOURTH_FIELD")
+	}
+	fifthField, err := decoder.Decode()
+	if err != nil {
+		t.Fatalf("err while decoding: %v", err)
+	}
+	if fifthField.(float64) != SIMPLE_MESSAGE_FIFTH_FIELD {
+		t.Fatalf("Decoded fifthField != SIMPLE_MESSAGE_FIFTH_FIELD")
 	}
 }
