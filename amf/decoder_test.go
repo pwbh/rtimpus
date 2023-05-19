abc0677c92cd16458f4e26f08e474efbaa0ca0a3
pwbh <127856937+pwbh@users.noreply.github.com>
2023-05-19 16:15:38 +0300

    completed complex test for decoding amf0

--- amf/decoder_test.go
+++ amf/decoder_test.go
@@ -9,19 +9,30 @@ const (
 	SIMPLE_MESSAGE_FIRST_FIELD  = "_result"
 	SIMPLE_MESSAGE_SECOND_FIELD = "Hello World!"
 	SIMPLE_MESSAGE_THIRD_FIELD  = "This is another test"
-	SIMPLE_MESSAGE_FOURTH_FIELD = float64(10_252)
-	SIMPLE_MESSAGE_FIFTH_FIELD  = float64(50)
+	SIMPLE_MESSAGE_FOURTH_FIELD = 10_252
+	SIMPLE_MESSAGE_FIFTH_FIELD  = 50.0003
 )
 
 const (
 	COMPLEX_MESSAGE_FIRST_FIELD  = "_result"
-	COMPLEX_MESSAGE_SECOND_FIELD = float64(1)
+	COMPLEX_MESSAGE_SECOND_FIELD = "version"
+	COMPLEX_MESSAGE_THIRD_FIELD  = 32653.12414
+	COMPLEX_MESSAGE_FOURTH_FIELD = "other_ObJeCt"
 
-	COMPLEX_MESSAGE_THIRD_FIELD_OBJECT_FIRST_VALUE                     = 3
-	COMPLEX_MESSAGE_THIRD_FIELD_OBJECT_SECOND_VALUE                    = "whatever string we want"
-	COMPLEX_MESSAGE_THIRD_FIELD_OBJECT_THIRD_VALUE                     = "https://localhost:10000/"
-	COMPLEX_MESSAGE_THIRD_FIELD_OBJECT_FOURTH_VALUE_OBJECT_FIRST_VALUE = "test"
-	COMPLEX_MESSAGE_THIRD_FIELD_OBJECT_FIFTH_VALUE                     = float64(2.0)
+	COMPLEX_MESSAGE_OBJECT_FIRST_KEY    = "scvWS"
+	COMPLEX_MESSAGE_OBJECT_FIRST_VALUE  = "https://localhost:10000/"
+	COMPLEX_MESSAGE_OBJECT_SECOND_KEY   = "test"
+	COMPLEX_MESSAGE_OBJECT_SECOND_VALUE = "test"
+	COMPLEX_MESSAGE_OBJECT_THIRD_KEY    = "another_test"
+	COMPLEX_MESSAGE_OBJECT_THIRD_VALUE  = 50.1231
+
+	COMPLEX_MESSAGE_OBJECT_FOURTH_KEY = "innerObj"
+
+	COMPLEX_MESSAGE_OBJECT_FOURTH_KEY_INNER_OBJECT_FIRST_KEY   = "test"
+	COMPLEX_MESSAGE_OBJECT_FOURTH_KEY_INNER_OBJECT_FIRST_VALUE = "test"
+
+	COMPLEX_MESSAGE_FIFTH_KEY   = "otherKey"
+	COMPLEX_MESSAGE_FIFTH_VALUE = 2
 )
 
 func amf0SimpleMessage() ([]byte, error) {
@@ -54,12 +65,17 @@ func amf0ComplexMessage() ([]byte, error) {
 	if err := amf0Encoder.Encode(COMPLEX_MESSAGE_SECOND_FIELD); err != nil {
 		return nil, err
 	}
-	complexObject := Object{
-		"version":    COMPLEX_MESSAGE_THIRD_FIELD_OBJECT_FIRST_VALUE,
-		"something":  COMPLEX_MESSAGE_THIRD_FIELD_OBJECT_SECOND_VALUE,
-		"swVcs":      COMPLEX_MESSAGE_THIRD_FIELD_OBJECT_THIRD_VALUE,
-		"some_else":  Object{"test": COMPLEX_MESSAGE_THIRD_FIELD_OBJECT_FOURTH_VALUE_OBJECT_FIRST_VALUE},
-		"sometrhing": COMPLEX_MESSAGE_THIRD_FIELD_OBJECT_FIFTH_VALUE}
+	if err := amf0Encoder.Encode(COMPLEX_MESSAGE_THIRD_FIELD); err != nil {
+		return nil, err
+	}
+	complexObject := make(Object)
+	complexObject[COMPLEX_MESSAGE_OBJECT_FIRST_KEY] = COMPLEX_MESSAGE_OBJECT_FIRST_VALUE
+	complexObject[COMPLEX_MESSAGE_OBJECT_SECOND_KEY] = COMPLEX_MESSAGE_OBJECT_SECOND_VALUE
+	complexObject[COMPLEX_MESSAGE_OBJECT_THIRD_KEY] = COMPLEX_MESSAGE_OBJECT_THIRD_VALUE
+	innerObject := make(Object)
+	innerObject[COMPLEX_MESSAGE_OBJECT_FOURTH_KEY_INNER_OBJECT_FIRST_KEY] = COMPLEX_MESSAGE_OBJECT_FOURTH_KEY_INNER_OBJECT_FIRST_VALUE
+	complexObject[COMPLEX_MESSAGE_OBJECT_FOURTH_KEY] = innerObject
+	complexObject[COMPLEX_MESSAGE_FIFTH_KEY] = COMPLEX_MESSAGE_FIFTH_VALUE
 	if err := amf0Encoder.Encode(complexObject); err != nil {
 		return nil, err
 	}
@@ -98,14 +114,14 @@ func TestAMF0DecoderDecodesSimpleMessage(t *testing.T) {
 	if err != nil {
 		t.Fatalf("err while decoding: %v", err)
 	}
-	if fourthField.(float64) != SIMPLE_MESSAGE_FOURTH_FIELD {
+	if fourthField.(float64) != float64(SIMPLE_MESSAGE_FOURTH_FIELD) {
 		t.Fatalf("Decoded fourthField != SIMPLE_MESSAGE_FOURTH_FIELD")
 	}
 	fifthField, err := decoder.Decode()
 	if err != nil {
 		t.Fatalf("err while decoding: %v", err)
 	}
-	if fifthField.(float64) != SIMPLE_MESSAGE_FIFTH_FIELD {
+	if fifthField.(float64) != float64(SIMPLE_MESSAGE_FIFTH_FIELD) {
 		t.Fatalf("Decoded fifthField != SIMPLE_MESSAGE_FIFTH_FIELD")
 	}
 }
@@ -124,11 +140,45 @@ func TestAMF0DecoderDecodesComplexMessage(t *testing.T) {
 	if firstField.(string) != COMPLEX_MESSAGE_FIRST_FIELD {
 		t.Fatalf("Decoded firstField != COMPLEX_MESSAGE_FIRST_FIELD")
 	}
-	s, err := decoder.Decode()
+	secondField, err := decoder.Decode()
 	if err != nil {
 		t.Fatalf("err while decoding: %v", err)
 	}
-	if s.(float64) != COMPLEX_MESSAGE_SECOND_FIELD {
-		t.Fatalf("Decoded s != COMPLEX_MESSAGE_SECOND_FIELD")
+	if secondField.(string) != COMPLEX_MESSAGE_SECOND_FIELD {
+		t.Fatalf("Decoded secondField != COMPLEX_MESSAGE_SECOND_FIELD")
+	}
+	thirdField, err := decoder.Decode()
+	if err != nil {
+		t.Fatalf("err while decoding: %v", err)
+	}
+	if thirdField.(float64) != COMPLEX_MESSAGE_THIRD_FIELD {
+		t.Fatalf("Decoded thirdField != COMPLEX_MESSAGE_THIRD_FIELD")
+	}
+	fourthField, err := decoder.Decode()
+	if err != nil {
+		t.Fatalf("err while decoding: %v", err)
+	}
+	if v, ok := fourthField.(Object); ok {
+		if v[COMPLEX_MESSAGE_OBJECT_FIRST_KEY].(string) != COMPLEX_MESSAGE_OBJECT_FIRST_VALUE {
+			t.Fatalf(`Decoded v[COMPLEX_MESSAGE_THIRD_FIELD_OBJECT_FIRST_KEY] != COMPLEX_MESSAGE_OBJECT_FIRST_VALUE`)
+		}
+		if v[COMPLEX_MESSAGE_OBJECT_SECOND_KEY].(string) != COMPLEX_MESSAGE_OBJECT_SECOND_VALUE {
+			t.Fatalf(`Decoded v[COMPLEX_MESSAGE_OBJECT_SECOND_KEY] != COMPLEX_MESSAGE_OBJECT_SECOND_VALUE`)
+		}
+		if v[COMPLEX_MESSAGE_OBJECT_THIRD_KEY].(float64) != COMPLEX_MESSAGE_OBJECT_THIRD_VALUE {
+			t.Fatalf(`Decoded v[COMPLEX_MESSAGE_OBJECT_THIRD_KEY] != COMPLEX_MESSAGE_OBJECT_THIRD_VALUE`)
+		}
+		if innerObj, ok := v[COMPLEX_MESSAGE_OBJECT_FOURTH_KEY].(Object); ok {
+			if innerObj[COMPLEX_MESSAGE_OBJECT_FOURTH_KEY_INNER_OBJECT_FIRST_KEY].(string) != COMPLEX_MESSAGE_OBJECT_FOURTH_KEY_INNER_OBJECT_FIRST_VALUE {
+				t.Fatalf(`Decoded innerObj[COMPLEX_MESSAGE_OBJECT_FOURTH_KEY_INNER_OBJECT_FIRST_KEY] != COMPLEX_MESSAGE_OBJECT_FOURTH_KEY_INNER_OBJECT_FIRST_VALUE`)
+			}
+		} else {
+			t.Fatalf(`Type of v[COMPLEX_MESSAGE_OBJECT_FOURTH_KEY] != Object`)
+		}
+		if v[COMPLEX_MESSAGE_FIFTH_KEY].(float64) != COMPLEX_MESSAGE_FIFTH_VALUE {
+			t.Fatalf(`Decoded v[COMPLEX_MESSAGE_FIFTH_KEY] != COMPLEX_MESSAGE_FIFTH_VALUE`)
+		}
+	} else {
+		t.Fatalf("Decoded thirdField != Object")
 	}
 }
