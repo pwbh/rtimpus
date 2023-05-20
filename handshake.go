d06fe8c88988c84ca721f80bbbc7e521defbd29e
pwbh <127856937+pwbh@users.noreply.github.com>
2023-05-20 10:23:10 +0300

    Cleanup handshake.go

--- handshake.go
+++ handshake.go
@@ -1,8 +1,8 @@
-package rtimpus
+//	Version defined by this specification is 3 (This is the only version this protocol will
+//	support 0-2 are depercated values sused by earleir proprietary products,
+//	4-31 are reserved for future implementations, 32-35 are not allowed.)
 
-import (
-	"fmt"
-)
+package rtimpus
 
 type Phase byte
 
@@ -12,23 +12,3 @@ const (
 	AckSent
 	HandshakeDone
 )
-
-type C0S0 struct {
-	Version int
-}
-
-func Trying() {
-	fmt.Println("Test")
-}
-
-//	Version defined by this specification is 3 (This is the only version this protocol will
-//	support 0-2 are depercated values sused by earleir proprietary products,
-//	4-31 are reserved for future implementations, 32-35 are not allowed.)
-
-//	TODO: Handeshake
-// 		C0 - RTMP version requestby the client from the server.
-// 			S0 - Selected version of the RTMP version from the server.
-// 				If server doesn't recognize the version sent by the client in C0
-// 				server should respond to the client with 3. Client may choose
-//				to degrade to version 3, or to abandon the handshake.
-// 		C1 - q
